e41d6ae44c38d9d36f9e19e9c3ecf099b61519a4
zloken <jan.lundmark@knowit.se>
2024-01-16 12:19:53 +0100

    replaced submodule with forked p-net

---
+++ cmake/RaspberryPi.cmake
@@ -0,0 +1,23 @@
+
+# RaspberryPi.cmake
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv7 -mfloat-abi=hard -mfpu=neon-vfpv4")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv7 -mfloat-abi=hard -mfpu=neon-vfpv4")
+
+# set(CMAKE_SYSROOT /usr)
+
+set(CMAKE_STAGING_PREFIX ./install)
+
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --sysroot=${CMAKE_SYSROOT} -static")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --sysroot=${CMAKE_SYSROOT} -static")
+set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} --sysroot=${CMAKE_SYSROOT} -static")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
